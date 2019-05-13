0a909f028baa6911e0aa8428ad2764a18800d472
AlmanInc <sevhwarang@gmail.com>
2019-05-13 15:45:38 +0300

    Implemented enemy logic (after lucky shot)

--- BASE/Sea Battle/Assets/Scripts/EnemyStepLogic.cs
+++ BASE/Sea Battle/Assets/Scripts/EnemyStepLogic.cs
@@ -10,7 +10,18 @@ public class EnemyStepLogic
         Error
     }
 
+    private enum Directions
+    {
+        None,
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
     private List<Vector2Int> _targetShips;
+    private Vector2Int _nextTargetPoint;
+    private Directions _lastDirection;
 
     public StepStates StepState { get; set; }
         
@@ -22,7 +33,10 @@ public class EnemyStepLogic
 
     public Vector2Int GetTargetPoint(PlayerBattleField field)
     {
-        return GetRandomPoint(field);
+        if (this.StepState == StepStates.First || _targetShips.Count == 0)
+            return GetRandomPoint(field);
+        else
+            return GetShipTargetPoint(field);
     }
 
     private Vector2Int GetRandomPoint(PlayerBattleField field)
@@ -38,6 +52,9 @@ public class EnemyStepLogic
             target.x = line.list[Random.Range(0, line.list.Count)];
             target.y = line.index;
 
+            if (field.FieldFilling[target.x, target.y] == FillTypes.Ship)
+                _targetShips.Add(target);
+
             this.StepState = StepStates.Correct;
         }
         else
@@ -48,8 +65,112 @@ public class EnemyStepLogic
 
     private Vector2Int GetShipTargetPoint(PlayerBattleField field)
     {
-        Vector2Int target = Vector2Int.zero;
+        if (_targetShips.Count == 0)
+        {
+            Debug.LogError("Null target");
+            return GetRandomPoint(field);
+        }
 
-        return target;
+        Vector2Int point = _targetShips[0];
+        Ship targetShip = field.FindShip(point.x, point.y);
+
+        if (targetShip == null)
+        {
+            _targetShips.RemoveAt(0);
+            _lastDirection = Directions.None;
+            return GetRandomPoint(field);
+        }
+
+        List<Directions> directions = new List<Directions>() { Directions.Left, Directions.Right, Directions.Up, Directions.Down };
+        Directions dir = _lastDirection == Directions.None ? directions[Random.Range(0, directions.Count)] : _lastDirection;
+        
+        while (directions.Count > 0)
+        {
+            if (CheckDirection(field, dir, point, targetShip))
+            {
+                if (targetShip.Lives <= 1 && field.FieldFilling[_nextTargetPoint.x, _nextTargetPoint.y] == FillTypes.Ship)
+                {
+                    _targetShips.RemoveAt(0);
+                    _lastDirection = Directions.None;
+                }
+
+                return _nextTargetPoint;
+            }
+            else
+            {
+                directions.Remove(dir);
+                dir = directions.Count > 0 ? directions[Random.Range(0, directions.Count)] : dir;
+            }
+        }
+        
+        return GetRandomPoint(field);
+    }
+
+    private bool CheckDirection(PlayerBattleField field, Directions direction, Vector2Int point, Ship ship)
+    {
+        switch (direction)
+        {
+            case Directions.Left:
+                for (int i = 1; i <= ship.Size.x && point.x - i >= 0; i++)
+                {
+                    var pos = field.FieldFilling[point.x - i, point.y];
+                    if (pos == FillTypes.Ship || pos == FillTypes.Empty || pos == FillTypes.NearShip)
+                    {
+                        _nextTargetPoint = new Vector2Int(point.x - i, point.y);
+                        _lastDirection = Directions.Left;
+                        return true;
+                    }
+                    else if (pos == FillTypes.Shot)
+                        break;
+                }
+                break;
+
+            case Directions.Right:
+                for (int i = 1; i <= ship.Size.x && point.x + i < field.Size.x; i++)
+                {
+                    var pos = field.FieldFilling[point.x + i, point.y];
+                    if (pos == FillTypes.Ship || pos == FillTypes.Empty || pos == FillTypes.NearShip)
+                    {
+                        _nextTargetPoint = new Vector2Int(point.x + i, point.y);
+                        _lastDirection = Directions.Right;
+                        return true;
+                    }
+                    else if (pos == FillTypes.Shot)
+                        break;
+                }
+                break;
+
+            case Directions.Up:
+                for (int i = 1; i <= ship.Size.y && point.y - i >= 0; i++)
+                {
+                    var pos = field.FieldFilling[point.x, point.y - i];
+                    if (pos == FillTypes.Ship || pos == FillTypes.Empty || pos == FillTypes.NearShip)
+                    {
+                        _nextTargetPoint = new Vector2Int(point.x, point.y - i);
+                        _lastDirection = Directions.Up;
+                        return true;
+                    }
+                    else if (pos == FillTypes.Shot)
+                        break;
+                }
+                break;
+
+            case Directions.Down:
+                for (int i = 1; i <= ship.Size.y && point.y + i < field.Size.y; i++)
+                {
+                    var pos = field.FieldFilling[point.x, point.y + i];
+                    if (pos == FillTypes.Ship || pos == FillTypes.Empty || pos == FillTypes.NearShip)
+                    {
+                        _nextTargetPoint = new Vector2Int(point.x, point.y + i);
+                        _lastDirection = Directions.Down;
+                        return true;
+                    }
+                    else if (pos == FillTypes.Shot)
+                        break;
+                }
+                break;
+        }
+
+        return false;
     }
 }
